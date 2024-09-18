17d8e0cb11d6bf04e1f391d042d13f6864807771
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-18 18:32:32 +0700

    prak 1 no 8

--- Pertemuan 4/variabel_konstanta.php
+++ Pertemuan 4/variabel_konstanta.php
@@ -2,16 +2,16 @@
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
-echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.<br>";
 
-//prak1 no 4
+// prak1 no 4
 $benar = true;
 $salah = false;
-echo "Variabel benar: $benar, Variabel salah: $salah";
+echo "Variabel benar: $benar, Variabel salah: $salah.<br>";
 
 // prak1 no 7
 // mendefinisikan konstanta untuk nilai tetap
 define("NAMA_SITUS", "WebsiteKu.com");
 define('TAHUN_PENDIRIAN', 2023);
-echo "Selamat datang di " . NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . ".";
+echo "Selamat datang di " . NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . ".<br>";
 ?>
