cd1c370405a6aee3691580dd64239a2ed454abc3
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-18 20:16:40 +0700

    prak3 no 16-17

---
+++ Pertemuan 4/Praktikum3No16.php
@@ -0,0 +1,8 @@
+<?php
+$kursiRestoran = 45;
+$kursiTerisi = 28;
+$kursiKosong = $kursiRestoran - $kursiTerisi;
+$presentaseKosong = $kursiKosong / $kursiRestoran * 100;
+echo "Presentase kursi kosong pada restoran: $presentaseKosong%";
+
+?>
