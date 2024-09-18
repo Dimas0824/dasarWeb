daba1ba83ab65f32b07ab5512bbf81e2d093e202
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-18 18:52:41 +0700

    prak2 no10-11

--- Pertemuan 4/tipe_data.php
+++ Pertemuan 4/tipe_data.php
@@ -30,6 +30,11 @@ $namaBelakang = "Jakaria";
 $namaLengkap = "{$namaDepan} {$namaBelakang}";
 $namaLengkap2 = $namaDepan . '' . $namaBelakang;
 
+echo "Nama Depan: {$namaDepan}<br>";
+echo "Nama Belakang: {$namaBelakang}<br>";
+
+echo $namaLengkap;
+
 ?>
 
 -
