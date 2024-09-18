e2b7325c495b7bec395b4a43a0b658a6c3743219
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-18 18:21:01 +0700

    prak 1 no 4

--- Pertemuan 4/variabel_konstanta.php
+++ Pertemuan 4/variabel_konstanta.php
@@ -3,4 +3,7 @@ $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
 echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+$benar = true;
+$salah = false;
+echo "Variabel benar: $benar, Variabel salah: $salah";
 ?>
