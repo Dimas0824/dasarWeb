7974c5b48e24d1369d553cf25d582955953ad264
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-18 18:44:24 +0700

    prak 2

---
+++ Pertemuan 4/tipe_data.php
@@ -0,0 +1,17 @@
+<?php
+$a = 10;
+$b = 5;
+$c = $a + 5;
+$d = $b + (10*5);
+$e = $d -$c;
+
+echo "Variabel a: $a<br>";
+echo "Variabel b: $b<br>";
+echo "Variabel c: $c<br>";
+echo "Variabel d: $d<br>";
+echo "Variabel e: $e<br>";
+
+var_dump($e);
+?>
+
+;
