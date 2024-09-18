69ed8b797b223c89a6e4a8eac2bb066f2ed465d8
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-18 20:19:36 +0700

    perbaikan prak3 no16-17

--- Pertemuan 4/Praktikum3No16.php
+++ Pertemuan 4/Praktikum3No16.php
@@ -3,6 +3,6 @@ $kursiRestoran = 45;
 $kursiTerisi = 28;
 $kursiKosong = $kursiRestoran - $kursiTerisi;
 $presentaseKosong = $kursiKosong / $kursiRestoran * 100;
-echo "Presentase kursi kosong pada restoran: " . round($presentaseKosong) . "%";
+echo "Presentase kursi kosong pada restoran: " . round($presentaseKosong, 2) . "%";
 
 ?>
