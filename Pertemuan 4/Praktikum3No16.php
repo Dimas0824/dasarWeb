5a3a9aba1ac696f5368834f350ba485e5dab0d91
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-19 08:05:35 +0700

    deletel file praktikum3no16

--- Pertemuan 4/Praktikum3No16.php
+++
@@ -1,8 +0,0 @@
-<?php
-$kursiRestoran = 45;
-$kursiTerisi = 28;
-$kursiKosong = $kursiRestoran - $kursiTerisi;
-$presentaseKosong = $kursiKosong / $kursiRestoran * 100;
-echo "Presentase kursi kosong pada restoran: " . round($presentaseKosong, 2) . "%";
-
-?>
