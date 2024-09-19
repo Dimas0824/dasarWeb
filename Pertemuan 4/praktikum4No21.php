aedc127660ed988fdc8105cee398e1b60978fd36
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-19 07:12:22 +0700

    perbaikan code agar lebih rapi

--- Pertemuan 4/praktikum4No21.php
+++ Pertemuan 4/praktikum4No21.php
@@ -4,12 +4,9 @@ $nilaiSiswa = [85,92,58,64,90,55,88,79,70,96];
 rsort($nilaiSiswa);
 $skorTotal = 0;
 
-//menghapus nilai tertinggi
-array_shift($nilaiSiswa);
-array_shift($nilaiSiswa);
-//menghapus nilai terendah
-array_pop($nilaiSiswa);
-array_pop($nilaiSiswa);
+//menghapus nilai tertinggi dan terendah
+array_splice($nilaiSiswa, 0, 2);
+array_splice($nilaiSiswa, -2, 2);
 
 foreach($nilaiSiswa as $skor){
     $skorTotal += $skor;
