e5a42bbce165bfa80e7f5afa8572c05944d5a600
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-25 18:10:53 +0700

    prak1

---
+++ Pertemuan 5/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+//membuat fungsi
+function perkenalan(){
+    echo"Assalamualaikum, ";
+    echo"Perkenalkan, nama saya Dimas <br/>";
+    echo"Senang berkenalan dengan anda <br/>";
+}
+
+//mmemanggil fungsi yang sudah dibuat
+perkenalan();
+
+?>
