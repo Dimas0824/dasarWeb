ee066366a042f0b194c7c241e81c9306f6690204
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-25 19:17:18 +0700

    prak2 no 4-5

--- Pertemuan 5/rekursif2.php
+++ Pertemuan 5/rekursif2.php
@@ -1,5 +1,13 @@
 <?php
-for ($i = 1; $i <= 25; $i++){
-    echo"Perulangan ke-{$i} <br>";
+function tampilkanAngka(int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if($indeks <= $jumlah){
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
+
+tampilkanAngka(20);
+
 ?>
