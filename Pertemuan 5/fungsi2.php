09843c47bab248c0251d198b4098a0f65b49d46a
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-25 18:38:44 +0700

    prak1no13-14

--- Pertemuan 5/fungsi2.php
+++ Pertemuan 5/fungsi2.php
@@ -4,7 +4,16 @@ function hitungUmur($thnLahir, $thnSekarang){
     $umur = $thnSekarang - $thnLahir;
     return $umur;
 }
+function perkenalan($name, $salam="Assalamualaikum"){
+    echo $salam.", ";
+    echo"Perkenalkan, nama saya ".$name." <br/>";
 
-echo"Umur saya adalah " . hitungUmur(2004, 2024) . " tahun";
+    //memanggil fungsi lain
+    echo"Saya berusia " . hitungUmur(2004, 2024) . " tahun<br/>";
+    echo"Senang berkenalan dengan anda <br/>";
+}
+
+//memanggil fungsi perkenalan
+perkenalan("Dimas");
 
 ?>
