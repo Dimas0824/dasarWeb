9d6e81ca18c35f8b4c4f92f45cebf8e738202d52
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-30 20:41:04 +0700

    penambahan style internal pada output

--- Pertemuan-6/array_2.php
+++ Pertemuan-6/array_2.php
@@ -4,23 +4,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Data Dosen</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
 
+        table {
+            width: 50%;
+            margin: 20px auto;
+            border-collapse: collapse;
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+
+        th,
+        td {
+            padding: 10px;
+            border: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+    </style>
 </head>
 
 <body>
     <?php
-
     $dosen = [
         'nama' => 'Elok Nur Hamdana',
         'domisili' => 'Malang',
         'jenis_kelamin' => 'Perempuan'
     ];
-
-    echo "Nama: {$dosen['nama']} <br>";
-    echo "Domisili: {$dosen['domisili']} <br>";
-    echo "Jenis Kelamin: {$dosen['jenis_kelamin']} <br>";
     ?>
+
+    <table>
+        <tr>
+            <th>Nama</th>
+            <td><?php echo $dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <th>Domisili</th>
+            <td><?php echo $dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <th>Jenis Kelamin</th>
+            <td><?php echo $dosen['jenis_kelamin']; ?></td>
+        </tr>
+    </table>
 </body>
 
 </html>
