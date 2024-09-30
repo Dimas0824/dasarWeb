be232c8a1a4b5e76c6353b408aaa30559b9fd13c
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-30 20:27:22 +0700

    perbaikan typo

--- Pertemuan-6/array_1.php
+++ Pertemuan-6/array_1.php
@@ -8,7 +8,7 @@
 <body>
     <h2>Array Terindeks</h2>
     <?php
-    $Listdosen = ["Elok Nurm Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
 
     foreach ($Listdosen as $dosen) {
         echo $dosen . "<br>";
