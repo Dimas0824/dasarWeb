c20251342cf4e0b095474abfd88b0e2782811c73
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-30 20:37:15 +0700

    prak 2

---
+++ Pertemuan-6/array_2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+
+</head>
+
+<body>
+    <?php
+
+    $dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili' => 'Malang',
+        'jenis_kelamin' => 'Perempuan'
+    ];
+
+    echo "Nama: {$dosen['nama']} <br>";
+    echo "Domisili: {$dosen['domisili']} <br>";
+    echo "Jenis Kelamin: {$dosen['jenis_kelamin']} <br>";
+    ?>
+</body>
+
+</html>
