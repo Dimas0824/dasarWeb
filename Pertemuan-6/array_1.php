6a86e76b884174147d2adc0ca81f19455becaae8
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-09-30 20:25:58 +0700

    prak 1 no 1-2

---
+++ Pertemuan-6/array_1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    $Listdosen = ["Elok Nurm Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+    echo $Listdosen[2] . "<br>";
+    echo $Listdosen[0] . "<br>";
+    echo $Listdosen[1] . "<br>";
+    ?>
+</body>
+
+</html>
