930bfb93b77994afab454817c57675d00e65b205
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-02 15:56:28 +0700

    perbaikan kesalahan tulis

--- Pertemuan-6/array_3.php
+++ Pertemuan-6/array_3.php
@@ -5,24 +5,55 @@
 </head>
 
 <body>
-    <table style="width:100%">
+    <table>
         <tr>
-            <th>Title</th>
+            <th>Judul Film</th>
+            <th>Tahun</th>
             <th>Rating</th>
         </tr>
         <?php
         $movies = array(
             array("The Avengers", 2012, 8.1),
             array("Guardians of the Galaxy", 2014, 8.1),
-            array("Iron Man", 2008, 7.9),
+            array("Guardians of the Galaxy", 2014, 5.1),
+            array("Iron Man", 2000, 7.9)
         );
 
-        foreach ($movies as $movie) {
-            echo "<tr>";
-            echo "<td>" . $movie[0] . "</td>";
-            echo "<td>" . $movie[2] . "</td>";
-            echo "</tr>";
-        }
+        echo "<tr>";
+        echo "<td>" . $movie[0][0] . "</td>";
+        echo "<td>" . $movie[0][1] . "</td>";
+        echo "<td>" . $movie[0][2] . "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>" . $movie[1][0] . "</td>";
+        echo "<td>" . $movie[1][1] . "</td>";
+        echo "<td>" . $movie[1][2] . "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>" . $movie[0][0] . "</td>";
+        echo "<td>" . $movie[0][1] . "</td>";
+        echo "<td>" . $movie[0][2] . "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>" . $movie[1][0] . "</td>";
+        echo "<td>" . $movie[1][1] . "</td>";
+        echo "<td>" . $movie[1][2] . "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>" . $movie[2][0] . "</td>";
+        echo "<td>" . $movie[3][1] . "</td>";
+        echo "<td>" . $movie[2][2] . "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>" . $movie[3][0] . "</td>";
+        echo "<td>" . $movie[3][4] . "</td>";
+        echo "<td>" . $movie[3][2] . "</td>";
+        echo "</tr>";
         ?>
     </table>
 </body>
