30ba8bf09af28c27d014d9626efa4fe4f78f0db2
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-02 16:12:12 +0700

    memanggil 2x

---
+++ Pertemuan-6/praktik-php/fungsi.php
@@ -0,0 +1,11 @@
+<?php
+//membuat fungsi
+function perkenalan()
+{
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Dimas <br/>";
+    echo "Senang berkenalan dengan anda <br/>";
+}
+//mmemanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
