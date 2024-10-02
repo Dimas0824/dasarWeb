c2fbe36df5cdf3d7109bd6cfbb6e15dcc91b1cfc
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-02 18:06:08 +0700

    membalikkan perkata no 6-8

--- Pertemuan-6/praktik-php/string_3.php
+++ Pertemuan-6/praktik-php/string_3.php
@@ -1,4 +1,9 @@
 <?php
-
-$pesan = "Saya arek malang";
-echo strrev($pesan) . "<br>";
+$pesan = "saya arek malang";
+#ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerKata = explode(" ", $pesan);
+#ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+#gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+echo $pesan . "<br/>";
