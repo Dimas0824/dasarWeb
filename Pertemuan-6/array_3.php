0addf85002c63f5c1370dc188921aa7bff9ddd73
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-02 15:51:33 +0700

    praktikum 3

---
+++ Pertemuan-6/array_3.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+
+<body>
+    <table style="width:100%">
+        <tr>
+            <th>Title</th>
+            <th>Rating</th>
+        </tr>
+        <?php
+        $movies = array(
+            array("The Avengers", 2012, 8.1),
+            array("Guardians of the Galaxy", 2014, 8.1),
+            array("Iron Man", 2008, 7.9),
+        );
+
+        foreach ($movies as $movie) {
+            echo "<tr>";
+            echo "<td>" . $movie[0] . "</td>";
+            echo "<td>" . $movie[2] . "</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+
+</html>
