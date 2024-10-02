9df21b9f7aeb75054c9e68a63ba19ccedc6aeb7d
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-02 17:47:19 +0700

    praktikum 3 no 4-5\

--- Pertemuan-6/praktik-php/rekursif2.php
+++ Pertemuan-6/praktik-php/rekursif2.php
@@ -2,3 +2,18 @@
 for ($i = 1; $i <= 25; $i++) {
     echo "Perulangan ke-{$i} <br>";
 }
+?>
+
+<?php
+//atau menggunakan dibawah ini
+function tampilkanAngka(int $jumlah, int $indeks = 1)
+{
+    echo "Perulangan ke-{$indeks} <br>";
+
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+
+tampilkanAngka(20);
