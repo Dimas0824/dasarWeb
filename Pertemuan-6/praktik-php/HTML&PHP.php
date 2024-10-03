f5a4b578a34ec994d34646cb578e86bd44535596
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-03 07:03:43 +0700

    penggabungan PHP dan HTML

---
+++ Pertemuan-6/praktik-php/HTML&PHP.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Cara 01</title>
+</head>
+
+<body>
+    <p>Tanggal hari ini: <?php echo date("d M Y") ?></p>
+</body>
+
+</html>
+
+<?php
+echo '<html>';
+echo '<head><title>Cara02</title></head>';
+echo '<body>';
+echo '<p>Tanggal hari ini: ' . date('d M Y') . '</p>';
+echo '</body>';
+echo '</html>';
