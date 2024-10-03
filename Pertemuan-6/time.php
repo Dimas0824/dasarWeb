e7e2bf3e846467241b80526367cebb0ea11eeef2
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-03 07:17:53 +0700

    date and time

---
+++ Pertemuan-6/time.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <h3>Time</h3>
+    <?php
+    date_default_timezone_set("asia/jakarta");
+    echo date("h:i:sa");
+    ?>
+</body>
+
+</html>
