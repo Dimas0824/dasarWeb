9de65029e81a8aea26f4b1f1a782108887a6ec8e
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-04 10:21:48 +0700

    request

--- Pertemuan-6/post.php
+++ Pertemuan-6/post.php
@@ -10,7 +10,7 @@
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // collect value of input field
-        $name = $_POST['fname'];
+        $name = $_REQUEST['fname'];
         if (empty($name)) {
             echo "Name is empty";
         } else {
