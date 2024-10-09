684a56010f6f205c4ee5510fcd555e94fd762542
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-09 22:05:52 +0700

    praktikum 5 no 9-12

--- Pertemuan-7/regex.php
+++ Pertemuan-7/regex.php
@@ -24,4 +24,4 @@ $replacement = 'banana';
 $text = 'I Like apple pie';
 $new_text = preg_replace($pattern, $replacement, $text);
 
-echo $new_text; // output: I Like banana pie
+echo $new_text; // output: I Like banana piee
