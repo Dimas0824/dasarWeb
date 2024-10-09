bab97a89380f03a317869026b5cb81ab11f50a6f
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-09 21:59:12 +0700

    praktikum 5 no 5-8

--- Pertemuan-7/regex.php
+++ Pertemuan-7/regex.php
@@ -3,7 +3,16 @@ $pattern = '/[a-z]/';
 $text = 'This is a Sample Text.';
 
 if (preg_match($pattern, $text)) {
-    echo 'Huruf kecil ditemukan!';
+    echo 'Huruf kecil ditemukan!<br><br>';
 } else {
-    echo 'Tidak ada huruf kecil!';
+    echo 'Tidak ada huruf kecil!<br><br>';
+}
+//tambahan kode
+$pattern = '/[0-9]+/';
+$text = 'There are 123 apples.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo 'Cocokkan: ' . $matches[0];
+} else {
+    echo 'Tidak ada yang cocok';
 }
