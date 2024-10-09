3f0e55a2cfd52b2e2efd3a57ee3a1622ceffaec3
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-09 18:40:26 +0700

    praktikum 1 no 5-6

--- Pertemuan-7/isset.php
+++ Pertemuan-7/isset.php
@@ -3,5 +3,12 @@ $umur;
 if (isset($umur) && $umur >= 18) {
     echo "Anda sudah dewasa.";
 } else {
-    echo "Anda belum dewasa atau variabel umur tidak ditemukan.";
+    echo "Anda belum dewasa atau variabel umur tidak ditemukan.<br>";
+}
+
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama tidak ditemukan dalam array.";
 }
