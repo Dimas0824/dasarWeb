4d1b91804861b99c77990e468ab8e79fb6158b57
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-09 20:23:04 +0700

    praktikum 2 no 4-5

--- Pertemuan-7/empty.php
+++ Pertemuan-7/empty.php
@@ -2,7 +2,13 @@
 
 $myArray = array(); //array kosong
 if (empty($myArray)) {
-    echo "Array tidak terdefinisi atau kosong";
+    echo "Array tidak terdefinisi atau kosong. <br><br>";
 } else {
-    echo "Array terdefinisi dan tidak kosong";
+    echo "Array terdefinisi dan tidak kosong<br><br>";
+}
+
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.<br><br>";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.<br><br>";
 }
