f81cbf1bdfc41a8a47ff414c1a9eee492520dcfc
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-09 22:04:58 +0700

    praktikum 4 no 9-12

--- Pertemuan-7/regex.php
+++ Pertemuan-7/regex.php
@@ -7,12 +7,21 @@ if (preg_match($pattern, $text)) {
 } else {
     echo 'Tidak ada huruf kecil!<br><br>';
 }
-//tambahan kode
+
+//tambahan kode no 5-8
 $pattern = '/[0-9]+/';
 $text = 'There are 123 apples.';
 
 if (preg_match($pattern, $text, $matches)) {
-    echo 'Cocokkan: ' . $matches[0];
+    echo 'Cocokkan: ' . $matches[0] . '<br><br>';
 } else {
     echo 'Tidak ada yang cocok';
 }
+
+//tambahan kode 9-12
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I Like apple pie';
+$new_text = preg_replace($pattern, $replacement, $text);
+
+echo $new_text; // output: I Like banana pie
