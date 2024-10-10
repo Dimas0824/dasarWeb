8c09bc54c8c3f076bc993cc88e0e2318db82bcd6
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-10 08:02:36 +0700

    praktikum 7 no 1-4

---
+++ Pertemuan-7/form_validasi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Form input dengan Validasi</title>
+</head>
+
+<body>
+    <h1>Form inpput dengan Validasi</h1>
+    <form method="POST" action="proses_validasi.php">
+        <label for="nama">Nama:</label>
+        <input type="text" id="nama" name="nama">
+        <br>
+
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email">
+        <br>
+
+        <input type="submit" value="submit">
+    </form>
+</body>
+
+</html>
