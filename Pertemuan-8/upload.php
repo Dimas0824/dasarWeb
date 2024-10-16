8a1305845164f179c7f8a031b1a0544e851c9d2e
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-16 18:45:06 +0700

    Praktikum 1 pertanyaan 4

--- Pertemuan-8/upload.php
+++ Pertemuan-8/upload.php
@@ -1,11 +1,18 @@
 <?php
-if (isset($_POST['submit'])) {
-    $targetdir = "uploads/";
-    $targetfile = "$targetdir" . basename($_FILES['myfile']['name']);
+if (isset($_POST["submit"])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
+    $file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $allowed_extensions = ["jpg", "jpeg", "png", "gif"];
+    $max_size = 5 * 1024 * 1024;
 
-    if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) {
-        echo "File berhasil diunggah";
+    if (in_array($file_type, $allowed_extensions) && $_FILES["myfile"]["size"] <= $max_size) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
+            echo "File berhasil diunggah.";
+        } else {
+            echo "Gagal mengunggah file.";
+        }
     } else {
-        echo "Gagal mengunggah file.";
+        echo "File tidak valid atau melebihi ukuran maksimum.";
     }
 }
