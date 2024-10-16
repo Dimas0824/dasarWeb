151d798adeee905ff47ad9be7028c22d51134f5d
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-16 21:25:27 +0700

    Perubahan hanya menerima upload gambar

--- Pertemuan-8/form_multiupload.php
+++ Pertemuan-8/form_multiupload.php
@@ -8,7 +8,8 @@
 <body>
     <h2>Unggah Dokumen</h2>
     <form action="proses_upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="files[]" multiple="multiple" accept=".pdf, .doc, .docx">
+        <!--input khusus gambar-->
+        <input type="file" name="files[]" multiple="multiple" accept=".jpeg, .jpg, .png, .gif">
         <input type="submit" name="Unggah">
     </form>
 </body>
