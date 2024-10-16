20652eabf7b7fd3a36ed54b28e7f1e19829785fe
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-16 18:31:04 +0700

    Upload foto

--- Pertemuan-8/upload.php
+++ Pertemuan-8/upload.php
@@ -1,9 +1,7 @@
 <?php
 if (isset($_POST['submit'])) {
-    $targetdir = __DIR__ . "uploads/";
+    $targetdir = "uploads/";
     $targetfile = "$targetdir" . basename($_FILES['myfile']['name']);
-
-    echo $targetdir;
 
     if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) {
         echo "File berhasil diunggah";
