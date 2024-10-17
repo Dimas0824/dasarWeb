2acf4503c53ff59da99b63a45d7e03d440356b6d
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-17 20:57:09 +0700

    praktikum 8 session

---
+++ Pertemuan-8/sessionCall.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    echo "Favorite color is " . $_SESSION["favcolor"] . "<br>";
+    echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+    ?>
+</body>
+
+</html>
