e2fae209814c0685f81ee128b2c66d9c228d0342
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-17 20:43:54 +0700

    praktikum 7

---
+++ Pertemuan-8/formBeli.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    <form action="prosesBeli.php" method="POST">
+        <p>Jumlah Novel yang dibeli :
+            <input type="text" name="beliNovel" value="0" size="2">
+        </p>
+        <p>Jumlah Buku Teks yang dibeli :
+            <input type="text" name="beliBuku" value="0" size="2">
+        </p>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
